9ab4d033863142a08ba7dbc91a268f1faa814075
gabriellima <gabrieeelima15@gmail.com>
2025-05-26 18:07:48 -0300

    Variable 'Robot'

--- src/main/java/me/gabrieeeo/main/Main.java
+++ src/main/java/me/gabrieeeo/main/Main.java
@@ -1,4 +1,15 @@
 package me.gabrieeeo.main;
 
+import java.awt.*;
+
 public class Main {
+
+    public static void main(String[] args) {
+        try {
+            Robot robot = new Robot();
+        }catch(AWTException e) {
+            System.out.println("Ocorreu uma exceção AWT: " + e.getMessage());
+        }
+    }
+
 }
