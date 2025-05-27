d93d8c09246c39f952a73c17ed2741330595b395
gabriellima <gabrieeelima15@gmail.com>
2025-05-27 16:39:33 -0300

    feat: new constructor with object initialize 'robot'

--- src/main/java/me/gabrieeeo/keyboard/KeyboardController.java
+++ src/main/java/me/gabrieeeo/keyboard/KeyboardController.java
@@ -1,4 +1,12 @@
 package me.gabrieeeo.keyboard;
 
+import java.awt.*;
+
 public class KeyboardController {
+
+    private final Robot robot;
+
+    public KeyboardController() throws AWTException {
+        this.robot = new Robot();
+    }
 }
