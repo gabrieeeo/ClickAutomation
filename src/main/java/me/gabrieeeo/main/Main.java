187f0d1f56ec2b3f9e2b946fd8459188e9ff5894
gabriellima <gabrieeelima15@gmail.com>
2025-05-27 11:14:17 -0300

    Adding method clicks em determinada posição

--- src/main/java/me/gabrieeeo/main/Main.java
+++ src/main/java/me/gabrieeeo/main/Main.java
@@ -1,25 +1,43 @@
 package me.gabrieeeo.main;
 
 import java.awt.*;
+import java.awt.event.InputEvent;
 import java.util.Timer;
 import java.util.TimerTask;
 
 public class Main {
 
-    public static void main(String[] args) {
-        Timer timer = new Timer();
+    final private static Timer timer = new Timer();
 
-        timer.schedule(new TimerTask() { //Agendar tarefa para ser executada a cada 5 segundos
+    public static void main(String[] args) {
+
+
+        //Agendar tarefa para ser executada a cada 5 segundos
+        timer.schedule(new TimerTask() {
             @Override
             public void run() {
 
-            }
-        },0, 5000);
+                //Executar cliques em determinada posição
+                try {
+                    Robot robot = new Robot();
+                    robot.mouseMove(887, 700); //Localização do mouse
+                    robot.mousePress(InputEvent.BUTTON1_DOWN_MASK); //Pressionar
+                    robot.mouseRelease(InputEvent.BUTTON1_DOWN_MASK); //Soltar
 
-        /*try {
-            Robot robot = new Robot();
-        }catch(AWTException e) {
-            System.out.println("Ocorreu uma exceção AWT: " + e.getMessage());
-        }*/
+                    //Após primeiro clique, seguir segundo clique
+                    try {
+                        Thread.sleep(500);
+                        robot.mouseMove(738, 600); //Localização do mouse
+                        robot.mousePress(InputEvent.BUTTON1_DOWN_MASK); //Pressionar
+                        robot.mouseRelease(InputEvent.BUTTON1_DOWN_MASK); //Soltar
+                    }catch (InterruptedException ie) {
+                        System.out.println("Ocorreu uma exceção Interrupted: " + ie.getMessage());
+                    }
+
+                }catch(AWTException e) {
+                    System.out.println("Ocorreu uma exceção AWT: " + e.getMessage());
+                }
+            }
+        },0, 1500);
     }
 }
