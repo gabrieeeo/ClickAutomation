fa38d7e092dfd052cfe84af0fb63f4c995e0547d
gabriellima <gabrieeelima15@gmail.com>
2025-05-27 16:07:35 -0300

    Refactoring code for easy view

--- src/main/java/me/gabrieeeo/mouse/MouseController.java
+++ src/main/java/me/gabrieeeo/mouse/MouseController.java
@@ -5,14 +5,14 @@ import java.awt.event.InputEvent;
 
 public class MouseController {
 
-    private static Robot robot;
+    private final Robot robot;
 
     //Construtor da classe chamado automaticamente (Estou adicionando o objeto Robot na classe MouseController)
     public MouseController() throws AWTException {
-        robot = new Robot();
+        this.robot = new Robot();
     }
 
-    public static void click(int x, int y) {
+    public void click(int x, int y) {
         robot.mouseMove(x, y); //Localização do mouse
         robot.mousePress(InputEvent.BUTTON1_DOWN_MASK); //Pressionar
         robot.mouseRelease(InputEvent.BUTTON1_DOWN_MASK); //Soltar
