cf81aa961dcd1a188f5637a02669d1651a29cda5
gabriellima <gabrieeelima15@gmail.com>
2025-05-27 09:04:04 -0300

    Adding timer schedule 5 seconds

--- src/main/java/me/gabrieeeo/main/Main.java
+++ src/main/java/me/gabrieeeo/main/Main.java
@@ -1,14 +1,25 @@
 package me.gabrieeeo.main;
 
 import java.awt.*;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class Main {
 
     public static void main(String[] args) {
-        try {
+        Timer timer = new Timer();
+
+        timer.schedule(new TimerTask() { //Agendar tarefa para ser executada a cada 5 segundos
+            @Override
+            public void run() {
+
+            }
+        },0, 5000);
+
+        /*try {
             Robot robot = new Robot();
         }catch(AWTException e) {
             System.out.println("Ocorreu uma exceção AWT: " + e.getMessage());
-        }
+        }*/
     }
 }
