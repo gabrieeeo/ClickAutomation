0b0de2c39ee69c530ee304cfe9c7853df0e8e352
gabriellima <gabrieeelima15@gmail.com>
2025-05-27 15:51:45 -0300

    Included method mouse click in Main

--- src/main/java/me/gabrieeeo/main/Main.java
+++ src/main/java/me/gabrieeeo/main/Main.java
@@ -14,21 +14,25 @@ public class Main {
 
     public static void main(String[] args) {
 
-        //Agendar tarefa para ser executada a cada 5 segundos
+        //Inicializar 'MouseController'
+        try {
+            mouseController = new MouseController();
+        } catch (AWTException e) {
+            System.out.println("Error intialize 'MouseController':" + e.getMessage());
+            return;
+        }
+
+        //Agendar tarefa para ser executada a cada 1.5 segundos
         timer.schedule(new TimerTask() {
             @Override
             public void run() {
-
                 //Executar cliques em determinada posição
                 try {
-                    mouseController = new MouseController();
                     mouseController.click(887, 700);
                     Thread.sleep(500);
                     mouseController.click(738, 600);
                 }catch (InterruptedException e) {
                     System.out.println("Thread error: " + e.getMessage());
-                } catch (AWTException e) {
-                    System.out.println("Thread merror: " + e.getMessage());
                 }
             }
         },0, 1500);
