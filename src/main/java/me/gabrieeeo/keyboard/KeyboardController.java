f373e06522834a3f375ff08296ce7c6e041abb32
Gabriel <gabrieeelima15@gmail.com>
2025-05-27 23:30:01 -0300

    feat: include 'else' in 'nativeKeyPressed'

--- src/main/java/me/gabrieeeo/keyboard/KeyboardController.java
+++ src/main/java/me/gabrieeeo/keyboard/KeyboardController.java
@@ -5,21 +5,19 @@ import com.github.kwhat.jnativehook.NativeHookException;
 import com.github.kwhat.jnativehook.keyboard.NativeKeyEvent;
 import com.github.kwhat.jnativehook.keyboard.NativeKeyListener;
 
-import java.awt.*;
-import java.awt.event.InputEvent;
-
 public class KeyboardController implements NativeKeyListener {
 
     public void nativeKeyPressed(NativeKeyEvent e) {
-        System.out.println("Pressione a letra 'Q' para encerrar a automação.");
         if (e.getKeyCode() == NativeKeyEvent.VC_Q) {
-            System.out.println("Você pressionou Q. Encerrando...");
+            System.out.println("Você pressionou a tecla 'Q'. Encerrando automação...");
             try {
                 GlobalScreen.unregisterNativeHook();
             } catch (NativeHookException ex) {
                 System.out.println("Error unregister Native Hook: " + ex.getMessage());
             }
             System.exit(0);
+        }else {
+            System.out.println("Pressione a letra 'Q' para encerrar a automação.");
         }
     }
 }
